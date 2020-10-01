abe9c76ce59a0663838fb23a4fb041c02c4c6a74
Boris Starkov <boris.starkow@gmail.com>
2020-10-01 21:44:31 +0300

    add /start handling

--- tgbot/app.py
+++ tgbot/app.py
@@ -4,6 +4,10 @@ import json
 import requests
 
 
+def send_message(token, chat_id, message):
+    requests.get('https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(token, chat_id, message))
+
+
 def lambda_handler(event, context):
 
     api_url = os.environ['SUDOKU_API_URL']
@@ -12,13 +16,20 @@ def lambda_handler(event, context):
     body = event['body']
     message = json.loads(body)['message']
 
-    chatId = message['chat']['id']
+    chat_id = message['chat']['id']
     text = message['text']
 
+    if text == '/start':
+        send_message(bot_token, chat_id, 'Please, enter 81 digit (e.g. 530070000600195000098000060800060003400803001700020006060000280000419005000080079)')
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "ok": "1"
+            }),
+        }
+
     result = requests.get(api_url, params={'data':text}).text
-
-    requests.get('https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(bot_token, chatId, result))
-
+    send_message(bot_token, chat_id, result)
     return {
         "statusCode": 200,
         "body": json.dumps({
